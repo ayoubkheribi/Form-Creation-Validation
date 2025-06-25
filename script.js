e8a5c64ac1631641dbae8c6ddbe4159070f3f535
Ayoub <akhribi627@gmail.com>
2025-06-25 03:06:31 +0100

    i added the files and the form

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-DOMContentLoaded();
+document.addEventListener('DOMContentLoaded', function() {
+
   const form = document.getElementById("registration-form");
   const feedbackDiv = document.getElementById("form-feedback");
 
-  form.addEventListener(`submit`, function () {
+  form.addEventListener(`submit`, (event) => {
     event.preventDefault();
   })
-
-  const username = document.getElementById("username").Value.trim(" ");
-  const email = document.getElementById("email").Value.trim(" ");
-  const password = document.getElementById("password").Value.trim(" ");
+  
+  const username = document.getElementById("username").Value.trim();
+  const email = document.getElementById("email").Value.trim();
+  const password = document.getElementById("password").Value.trim();
 
   let isValid = true;
   let messages = [];
 
   if (username.length < 3) {
     isValid = false;
-    masseges.push("your username not valid");
+    masseges.push("your username's not valid");
   }
   else{
-    masseges = "";
+    masseges.push("")
   }
+
+  if (email.include("@", ".")) {
+    isValid = false;
+    masseges.push("your email is not valid");
+  }
+  else{
+    masseges.push("")
+  }
+
+  if (password.length < 8) {
+    isValid = false;
+    masseges.push("your password is not long enough");
+  }
+  else{
+    masseges.push("")
+  }
+
+feedbackDiv.style.display = "block";
+
+  if(isValid){
+        feedbackDiv.textContent = "Registration successful";
+        feedbackDiv.style.color = "#28a745";
+
+    } else {
+        feedbackDiv.innerHTML = messages.join("<br>")
+        feedbackDiv.style.color = "#dc3545";
+    }
+})
