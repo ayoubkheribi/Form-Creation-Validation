f64046c5f33fe4ad38846e3082362d3018fb8367
Ayoub <akhribi627@gmail.com>
2025-06-26 00:39:42 +0100

    add the quiz.js

---
+++ fetch-data.js
@@ -0,0 +1,13 @@
+async function fetchUserData() {
+  const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+  const dataContainer = document.getElementById('api-data');
+  try{
+    const response = await fetch(apiUrl);
+    const users = await response.json();
+    dataContainer.innerHTML = '';
+  }
+  catch{
+
+  }
+
+}
